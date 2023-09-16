244b5cdcc229275cea46caf2e4640e396b2ae7bd
Fábio Pereira da Silva <fabioegel@gmail.com>
2023-09-16 14:01:30 -0300

    Implementing NodeJS addon with DEBUG_MODE=ON

--- src/napi/addon.c
+++ src/napi/addon.c
@@ -39,8 +39,29 @@ c=a.create()
   #error "Error. Declare first line below in stdsoap2.h #define WITH_NOIDREF"
 #endif
 
+#ifdef JS_SOAP_DEBUG
+ #define JS_WITSML21_DEBUG(...) \
+    fprintf(stdout, __VA_ARGS__);
+#else
+ #define JS_WITSML21_DEBUG(std, ...)
+#endif
+
+#define JS_CWS_THROW(errFunc, errDesc, errCode) \
+  cws_js_throw(env, &cws_js_err, errFunc, errDesc, errCode); \
+  return NULL;
+
+#define JS_CWS_THROW_COND(condT, errFunc, errDesc, errCode) \
+  if (condT) { \
+    JS_CWS_THROW(errFunc, errDesc, errCode) \
+  }
+
 /// UTILITIES
 typedef napi_value (*cws_node_fn)(napi_env, napi_callback_info);
+
+struct js_cws_config_t {
+  struct soap *soap_internal;
+  int err;
+};
 
 struct cws_js_err_t {
   char buf[256];
@@ -53,15 +74,71 @@ typedef struct cws_js_fn_call_t {
    cws_node_fn fn;
 } CWS_JS_FUNCTIONS_OBJ;
 
+static void js_cws_config_free(struct js_cws_config_t **js_cws_config)
+{
+  CWS_CONFIG *config;
+
+  JS_WITSML21_DEBUG("\njs_cws_config_free: Begin free if is NOT NULL %p", *js_cws_config)
+
+  if (*js_cws_config) {
+    config=(CWS_CONFIG *)(*js_cws_config)->soap_internal->user;
+
+    JS_WITSML21_DEBUG("\njs_cws_config_free: Destroying soap_internal %p", (*js_cws_config)->soap_internal)
+    cws_internal_soap_free(&(*js_cws_config)->soap_internal);
+
+    JS_WITSML21_DEBUG("\njs_cws_config_free: Destroying config %p", config)
+    cws_config_free(&config);
+
+    JS_WITSML21_DEBUG("\njs_cws_config_free: Destroying js_cws_config object %p", js_cws_config)
+    free((void *)*js_cws_config);
+    *js_cws_config=NULL;
+
+    JS_WITSML21_DEBUG("\njs_cws_config_free: Destroyed")
+  }
+
+}
+
+static struct js_cws_config_t *js_cws_config_init()
+{
+  CWS_CONFIG *config;
+  struct js_cws_config_t *js_cws_config;
+
+  JS_WITSML21_DEBUG("\njs_cws_config_init: Initializing ...")
+  if (!(js_cws_config=(struct js_cws_config_t *)malloc(sizeof(struct js_cws_config_t))))
+    return NULL;
+
+  if (!(config=cws_config_new("JSWITSML 2.1 BSON parser")))
+    goto js_cws_config_init_exit1;
+
+  if (cws_internal_soap_new(&js_cws_config->soap_internal, config, NULL))
+    goto js_cws_config_init_exit2;
+
+  js_cws_config->err=0;
+
+  JS_WITSML21_DEBUG("\njs_cws_config_init: SUCCESS ... %p", js_cws_config)
+
+  return js_cws_config;
+
+js_cws_config_init_exit2:
+  cws_config_free(&config);
+
+js_cws_config_init_exit1:
+  free((void *)js_cws_config);
+
+  JS_WITSML21_DEBUG("\njs_cws_config_init: FAIL ...")
+
+  return NULL;
+}
+
 //https://nodejs.org/api/n-api.html#napi_finalize
 //typedef void (*napi_finalize)(napi_env env, void* finalize_data, void* finalize_hint);
 static void cws_js_finalize(napi_env env, void *finalize_data, void* finalize_hint)
 {
-  printf("\nFinalize %p %s", finalize_data, (char *)finalize_hint);
-  if (finalize_data) {
-    printf("Freeing finalize_data ...\n");
-    free(finalize_data);
-  }
+  void *data=finalize_data;
+  JS_WITSML21_DEBUG("\ncws_js_finalize: Entering and perform cleanup %p", data)
+
+  js_cws_config_free((struct js_cws_config_t **)&data);
+
 }
 
 static void cws_js_throw(napi_env env, struct cws_js_err_t *caster, const char *c_function_name, const char *errMessage, int err)
@@ -101,11 +178,11 @@ napi_value c_getBsonVersion(napi_env env, napi_callback_info info)
 {
   napi_value argv, res;
   size_t argc=1;
-  void *thisData, *buf;
+  void *buf;
   struct cws_js_err_t cws_js_err;
   struct cws_version_t version;
 
-  if (napi_get_cb_info(env, info, &argc, &argv, NULL, &thisData)!=napi_ok) {
+  if (napi_get_cb_info(env, info, &argc, &argv, NULL, NULL)!=napi_ok) {
     cws_js_throw(env, &cws_js_err, "c_getBsonVersion", "Can't parse arguments", -19);
     return NULL;
   }
@@ -117,17 +194,8 @@ napi_value c_getBsonVersion(napi_env env, napi_callback_info info)
 
   cws_version(&version);
 
-/*
-#define TEST "Testing module"
-  if (napi_create_string_utf8(env, (const char *)TEST, sizeof(TEST)-1, &res)!=napi_ok) {
-    napi_throw_error(env, "-21", "c_version: Fail on parse UTF-8 string");
-    return NULL;
-  }
-*/
-
   if (napi_create_arraybuffer(env, version.versionSize, &buf, &res)==napi_ok) {
     memcpy(buf, (void *)version.version, version.versionSize);
-    printf("\nData pointer thisData %p\n", thisData);
     return res;
   }
 
@@ -144,82 +212,49 @@ CWS_JS_FUNCTIONS_OBJ CWS_JS_CREATE_FUNCTIONS[] = {
 napi_value c_create(napi_env env, napi_callback_info info)
 {
   int err;
-  napi_value thisArg, constructor, argv=NULL, res;
-  //size_t argc = 1;
+  napi_value constructor, argv=NULL, res;
   size_t argc=0;
+  struct js_cws_config_t *js_cws_instance;
   struct cws_js_err_t cws_js_err;
 
-  if (napi_get_cb_info(env, info, &argc, &argv, &thisArg, NULL)!=napi_ok) {
-    cws_js_throw(env, &cws_js_err, "c_create", "Can't parse arguments. Wrong argument at create", 29);
-    return NULL;
-  }
+  JS_CWS_THROW_COND(
+    napi_get_cb_info(env, info, &argc, &argv, NULL, NULL)!=napi_ok,
+    "napi_get_cb_info",
+    "Can't parse arguments. Wrong argument at create",
+    98
+  )
 
-  if (argc) {
-    cws_js_throw(env, &cws_js_err, "c_create", "Too many arguments", 30);
-    return NULL;
-  }
-/*
-  if (napi_get_global(env, &thisArg)!=napi_ok) {
-    cws_js_throw(env, &cws_js_err, "c_create", "Could not get global env", 31);
-    return NULL;  
-  }
-*/
-/*
-  if (napi_get_named_property(env, thisArg, "_witsml21bsonInit_", &constructor)!=napi_ok) {
-    cws_js_throw(env, &cws_js_err, "c_create", "Could not add '_witsml21bsonInit_' method to constructor", 32);
-    return NULL;  
-  }
-*/
+  JS_CWS_THROW_COND(argc, "c_create", "Too many arguments", 99)
 
-  if (napi_create_function(env, NULL, 0, c__witsml21bsonInit_, NULL, &constructor)!=napi_ok) {
-    cws_js_throw(env, &cws_js_err, "c_create", "napi_create_function error", 100);
-    return NULL;
-  }
+  JS_CWS_THROW_COND(
+    napi_create_function(env, NULL, 0, c__witsml21bsonInit_, NULL, &constructor)!=napi_ok,
+    "napi_create_function",
+    "Error on generating c__witsml21bsonInit_",
+    100
+  )
 
-/*
-  if ((err=cws_add_function_util(&constructor, env, thisArg, CWS_JS_CREATE_FUNCTIONS))) {
-    cws_js_throw(env, &cws_js_err, "c_create", "Could add functions to constructor", err);
-    return NULL;
-  }
-*/
   if (napi_new_instance(env, constructor, 0, &argv, &res)==napi_ok) {
 
-    void *v=malloc(32);
-    if ((err=cws_add_function_util(NULL, env, res, CWS_JS_CREATE_FUNCTIONS, v))) {
-      if (v)
-        free(v);
-      cws_js_throw(env, &cws_js_err, "c_createA", "Could add functions to constructor", err);
-      return NULL;
+    JS_CWS_THROW_COND((js_cws_instance=js_cws_config_init())==NULL, "js_cws_config_init", "Could not alloc JSWITSML 2.1 C instance", 101)
+
+    if ((err=cws_add_function_util(NULL, env, res, CWS_JS_CREATE_FUNCTIONS, js_cws_instance))) {
+      js_cws_config_free(&js_cws_instance);
+
+      JS_CWS_THROW("cws_add_function_util", "Could add functions @ c_create constructor", 102)
     }
 
-    printf("\nCreating ... %p", v);
+    if (napi_add_finalizer(env, res, (void *)js_cws_instance, cws_js_finalize, NULL, NULL)!=napi_ok) {
+      js_cws_config_free(&js_cws_instance);
 
-/*
-    if ((err=cws_add_function_util(NULL, env, res, CWS_JS_CREATE_FUNCTIONS))) {
-      cws_js_throw(env, &cws_js_err, "c_create", "Could add functions to constructor", err);
-      return NULL;
-    }
-*/
-
-    if (napi_add_finalizer(env, res, (void *)v, cws_js_finalize, (void *)"abc", NULL)!=napi_ok) {
-      cws_js_throw(env, &cws_js_err, "c_create", "Could not add finalizer callback", 33);
-      return NULL;
+      JS_CWS_THROW("napi_add_finalizer", "Could not add finalizer callback", 103)
     }
 
     return res;
   }
 
-  cws_js_throw(env, &cws_js_err, "c_create", "Could not create new instance", 34);
-  return NULL;
+  JS_CWS_THROW("c_create", "Could not create new instance", 104)
 }
 
-/*
-CWS_JS_FUNCTIONS_OBJ CWS_JS_INIT_FUNCTIONS[] = {
-   {"getBsonVersion", c_bson_version},
-   {"create", c_create_constructor},
-   {NULL}
-};
-*/
 CWS_JS_FUNCTIONS_OBJ CWS_JS_INIT_FUNCTIONS[] = {
    SET_JS_FN_CALL(create),
    {NULL}
@@ -227,21 +262,23 @@ CWS_JS_FUNCTIONS_OBJ CWS_JS_INIT_FUNCTIONS[] = {
 
 napi_value Init(napi_env env, napi_value exports)
 {
-
   int err;
-
   struct cws_js_err_t cws_js_err;
 
-  if ((err=cws_add_function_util(NULL, env, exports, CWS_JS_INIT_FUNCTIONS, NULL))) {
-    cws_js_throw(env, &cws_js_err, "cws_add_function_util", "Could not initialize C functions", err);
-    return NULL;
-  }
+  JS_CWS_THROW_COND(
+    (err=cws_add_function_util(NULL, env, exports, CWS_JS_INIT_FUNCTIONS, NULL)),
+    "cws_add_function_util",
+    "Could not initialize C functions",
+    err
+  )
 
-  if (napi_set_instance_data(env, (void *)NULL, cws_js_finalize, (void *)"test")!=napi_ok) {
+/*
+  TODO remove it. Maybe not necessary
+  if (napi_set_instance_data(env, (void *)NULL, cws_js_finalize, (void *)NULL)!=napi_ok) {
     cws_js_throw(env, &cws_js_err, "napi_set_instance_data", "Could not set instance data to object", err);
     return NULL;
   }
-
+*/
   return exports;
 }
 
